945810bd9f9c6220bcf84ef8c9dee90e8ac8092d
RQ <ricky.dwsystems@gmail.com>
2023-02-16 13:41:41 +0700

    fix bug in resetting cuti in Januari payroll

--- CAJWebApp/Areas/PAYROLL/Controllers/PayrollsController.cs
+++ CAJWebApp/Areas/PAYROLL/Controllers/PayrollsController.cs
@@ -675,7 +675,7 @@ namespace CAJWebApp.Areas.PAYROLL.Controllers
 
             //reset leave days on January
             if (payPeriod.Month == 1)
-                model.LeaveDaysStartingBalance = settings.LeaveDaysPerYear;
+                model.LeaveDaysAdjustment = settings.LeaveDaysPerYear - model.LeaveDaysStartingBalance;
 
             int totalMonthAfterJoin = model.PayPeriod.Month - model.JoinDate.Month + 12 * (model.PayPeriod.Year - model.JoinDate.Year);
             if (model.MandatoryDepositStartingBalance < settings.DepositAmountTotal)
